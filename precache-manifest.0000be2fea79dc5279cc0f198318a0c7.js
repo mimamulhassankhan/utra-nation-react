774dfc76527a856efae4b37f74829e10556c1d2a
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-07 16:01:03 +0600

    Updates

---
+++ precache-manifest.0000be2fea79dc5279cc0f198318a0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5b1066a5c3f55c14dd3cf7afc5b0b5",
+    "url": "/utra-nation-react/index.html"
+  },
+  {
+    "revision": "d6f0ee8e15a659abbb06",
+    "url": "/utra-nation-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75ecd75f679ffb26b3b4",
+    "url": "/utra-nation-react/static/js/2.b1b3f7f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/utra-nation-react/static/js/2.b1b3f7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f0ee8e15a659abbb06",
+    "url": "/utra-nation-react/static/js/main.3b5aa576.chunk.js"
+  },
+  {
+    "revision": "05768c77398e079f8f5f",
+    "url": "/utra-nation-react/static/js/runtime-main.114cf7b8.js"
+  }
+]);
